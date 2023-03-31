107ce8d04ff07b692e64ccda7a20e6c29a1b49af
Suraj5260 <suraj.telrandhe16@gmail.com>
2023-03-31 18:48:27 +0530

    start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "jquery";
+import "popper.js/dist/umd/popper";
+import "bootstrap/dist/js/bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import NavBar from "./App";
+
+createRoot(document.getElementById("root")).render(<NavBar />);
